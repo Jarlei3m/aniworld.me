744d9b335a8e6bc253777494d328b6634d6e11e6
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2021-08-30 10:17:49 -0300

    chore: basic hero carousel setup

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,34 +1,127 @@
 import { BsStar, BsStarFill, BsStarHalf } from 'react-icons/bs';
 import { Container } from './styles';
 
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 export function Hero() {
   return (
     <Container>
-      <img
-        src="https://c.wallhere.com/photos/4e/05/Elfen_Lied_Nyu-1351501.jpg!d"
-        alt=""
-      />
-      <article>
-        <h4>
-          Season 1 • <span>2005</span>
-        </h4>
-        <h1>Elfen Lied</h1>
-        <span className="stars">
-          <BsStarFill />
-          <BsStarFill />
-          <BsStarFill />
-          <BsStarHalf />
-          <BsStar />
-        </span>
-        <p>
-          Elfen Lied takes place in Kamakura and Kanagawa, and focuses on the
-          "Diclonius", a newly mutated species. Their appearance is similar to
-          humans, but with several differences, namely horn-like protrusions on
-          the forehead and the presence of telekinetic invisible arms called
-          "Vectors".
-        </p>
-        <button type="button">Watch</button>
-      </article>
+      <Carousel
+        autoPlay={true}
+        emulateTouch={true}
+        infiniteLoop={true}
+        interval={5000}
+        showStatus={false}
+      >
+        <div>
+          <img
+            src="https://c.wallhere.com/photos/4e/05/Elfen_Lied_Nyu-1351501.jpg!d"
+            alt=""
+          />
+          <article>
+            <h4>
+              Season 1 • <span>2005</span>
+            </h4>
+            <h1>Elfen Lied</h1>
+            <span className="stars">
+              <BsStarFill />
+              <BsStarFill />
+              <BsStarFill />
+              <BsStarHalf />
+              <BsStar />
+            </span>
+            <p>
+              Elfen Lied takes place in Kamakura and Kanagawa, and focuses on
+              the "Diclonius", a newly mutated species. Their appearance is
+              similar to humans, but with several differences, namely horn-like
+              protrusions on the forehead and the presence of telekinetic
+              invisible arms called "Vectors".
+            </p>
+            <button type="button">Watch</button>
+          </article>
+        </div>
+
+        <div>
+          <img src="https://wallpaperaccess.com/full/2412896.jpg" alt="" />
+          <article>
+            <h4>
+              Season 1 • <span>2005</span>
+            </h4>
+            <h1>Elfen Lied</h1>
+            <span className="stars">
+              <BsStarFill />
+              <BsStarFill />
+              <BsStarFill />
+              <BsStarHalf />
+              <BsStar />
+            </span>
+            <p>
+              Elfen Lied takes place in Kamakura and Kanagawa, and focuses on
+              the "Diclonius", a newly mutated species. Their appearance is
+              similar to humans, but with several differences, namely horn-like
+              protrusions on the forehead and the presence of telekinetic
+              invisible arms called "Vectors".
+            </p>
+            <button type="button">Watch</button>
+          </article>
+        </div>
+
+        <div>
+          <img
+            src="https://i.pinimg.com/originals/68/2b/13/682b1326d5d1b2c2e05ff2b63e22b3f6.jpg"
+            alt=""
+          />
+          <article>
+            <h4>
+              Season 1 • <span>2005</span>
+            </h4>
+            <h1>Elfen Lied</h1>
+            <span className="stars">
+              <BsStarFill />
+              <BsStarFill />
+              <BsStarFill />
+              <BsStarHalf />
+              <BsStar />
+            </span>
+            <p>
+              Elfen Lied takes place in Kamakura and Kanagawa, and focuses on
+              the "Diclonius", a newly mutated species. Their appearance is
+              similar to humans, but with several differences, namely horn-like
+              protrusions on the forehead and the presence of telekinetic
+              invisible arms called "Vectors".
+            </p>
+            <button type="button">Watch</button>
+          </article>
+        </div>
+        <div>
+          <img
+            src="https://preview.redd.it/lmfry5ppb2bx.png?width=960&crop=smart&auto=webp&s=19dd437cd9002a202c9711ff80702e67171ec56a"
+            alt=""
+          />
+          <article>
+            <h4>
+              Season 1 • <span>2005</span>
+            </h4>
+            <h1>Elfen Lied</h1>
+            <span className="stars">
+              <BsStarFill />
+              <BsStarFill />
+              <BsStarFill />
+              <BsStarHalf />
+              <BsStar />
+            </span>
+            <p>
+              Elfen Lied takes place in Kamakura and Kanagawa, and focuses on
+              the "Diclonius", a newly mutated species. Their appearance is
+              similar to humans, but with several differences, namely horn-like
+              protrusions on the forehead and the presence of telekinetic
+              invisible arms called "Vectors".
+            </p>
+            <button type="button">Watch</button>
+          </article>
+        </div>
+      </Carousel>
     </Container>
   );
 }
--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -10,67 +10,97 @@ export const Container = styled.div`
 
   box-shadow: 8px 8px 16px rgba(0, 0, 0, 0.4);
 
-  img {
-    width: 100%;
-    height: 100%;
+  .carousel-root {
+    max-height: 32rem;
 
-    object-fit: cover;
-    object-position: center;
-    filter: brightness(0.7) grayscale(70%);
-    -webkit-filter: brightness(0.7) grayscale(70%);
+    .carousel {
+      position: absolute;
+      max-height: 100%;
+
+      .thumbs-wrapper,
+      .control-arrow {
+        display: none;
+      }
+
+      .slider-wrapper.axis-horizontal {
+        transform: translateY(-10%);
+      }
+
+      ul {
+        margin: 1rem 0;
+
+        &.control-dots li.dot {
+          width: 0.725rem;
+          height: 0.725rem;
+        }
+      }
+    }
   }
 
-  article {
-    position: absolute;
-    top: 20%;
-    margin-left: 6rem;
-    width: 26rem;
+  div {
+    img {
+      width: 100%;
+      height: 100%;
 
-    h4 {
-      font-size: 0.925rem;
-      font-weight: 400;
+      object-fit: cover;
+      object-position: center;
+      filter: brightness(0.7) grayscale(70%);
+      -webkit-filter: brightness(0.7) grayscale(70%);
+    }
 
-      span {
+    article {
+      position: absolute;
+      top: 25%;
+      margin-left: 6rem;
+      text-align: left;
+      width: 26rem;
+
+      h4 {
+        font-size: 0.925rem;
+        font-weight: 400;
+
+        span {
+          color: var(--gray-300);
+        }
+      }
+
+      h1 {
+        font-size: 2.8rem;
+        font-weight: 900;
+        line-height: 4rem;
+        /* color: linear-gradient(to bottom, var(--white), #5c2121); */
+        background: -webkit-linear-gradient(var(--white), #5c2121);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+      }
+
+      svg {
+        color: var(--yellow-500);
+        & + svg {
+          margin-left: 0.2rem;
+        }
+      }
+
+      p {
+        margin: 1.2rem 0;
         color: var(--gray-300);
+        line-height: 1.2rem;
       }
-    }
 
-    h1 {
-      font-size: 2.8rem;
-      font-weight: 900;
-      line-height: 4rem;
-      /* color: linear-gradient(to bottom, var(--white), #5c2121); */
-      background: -webkit-linear-gradient(var(--white), #5c2121);
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-    }
+      button {
+        height: 3rem;
+        width: 10rem;
+        background: var(--white);
+        color: var(--green-300);
+        border: none;
+        border-radius: 16px;
+        font-weight: 700;
 
-    svg {
-      color: var(--yellow-500);
-      & + svg {
-        margin-left: 0.2rem;
-      }
-    }
+        transition: filter 0.2s;
 
-    p {
-      margin: 1.2rem 0;
-      color: var(--gray-300);
-      line-height: 1.2rem;
-    }
-
-    button {
-      height: 3rem;
-      width: 10rem;
-      background: var(--white);
-      color: var(--green-300);
-      border: none;
-      border-radius: 16px;
-      font-weight: 700;
-
-      transition: filter 0.2s;
-
-      &:hover {
-        filter: brightness(0.9);
+        &:hover {
+          filter: brightness(0.9);
+        }
       }
     }
   }
